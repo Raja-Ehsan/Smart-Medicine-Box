b0e296d17d7428b11cf2258626cb7385e8000cd3
rajaehsan1234 <91175440+rajaehsan1234@users.noreply.github.com>
2022-05-28 04:51:48 +0500

    Almost Completed..
    New feature: Setting alarm using AsyncWebServer, and some modifications too alarm functions and firebase function so that data is correctly entered into the database..

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,305 @@
 //----------Included Libraries---------
 #include <Arduino.h>
-#include <WiFi.h>
+#include "WiFi.h"
 #include "time.h"
-//Libaray that allows to send mail from ESP32 using your gmaill account
-#include <ESP_Mail_Client.h>
-//For Firebase connection with ESP-32
-#include <Firebase_ESP_Client.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+#include <ESP_Mail_Client.h>//Libaray that allows to send mail from ESP32 using your gmaill account
+#include <Firebase_ESP_Client.h>//For Firebase connection with ESP-32
+
+
+//-------tokens-------
 //Provide the token generation process info.
 #include "addons/TokenHelper.h"
 //Provide the RTDB payload printing info and other helper functions.
 #include "addons/RTDBHelper.h"
 
-//------definend Globals------
 
+//------definend Globals for Firebase Authentication------
 #define USER_EMAIL "rajaehsanriaz@gmail.com"
 #define USER_PASSWORD "Thelionking"
 #define API_KEY "AIzaSyCMFMYfeKKE5xifcep3wLcGE4xoh4GyUWc"
 // Insert RTDB URLefine the RTDB URL */
 #define DATABASE_URL "https://esp32-data-94dd1-default-rtdb.asia-southeast1.firebasedatabase.app/" 
-//wifi credentials
+
+//-----------wifi credentials-----------
 #define Wifi_net "AndroidAP"
 #define Wifi_pass "rajaamjad"
 #define Timeout 20000
-// The smtp host name e.g. smtp.gmail.com for GMail or smtp.office365.com for Outlook or smtp.mail.yahoo.com
-#define SMTP_HOST "smtp.gmail.com"
-// 465 or esp_mail_smtp_port_465
-#define SMTP_PORT 465
-// The sign in credentials 
+
+
+// --------The gmail credentials--------- 
+#define SMTP_HOST "smtp.gmail.com"// The smtp host name e.g. smtp.gmail.com for GMail or smtp.office365.com for Outlook or smtp.mail.yahoo.com
+#define SMTP_PORT 465// 465 or esp_mail_smtp_port_465
 #define AUTHOR_EMAIL "rajaehsanriaz@gmail.com"
-#define AUTHOR_PASSWORD "******"
+#define AUTHOR_PASSWORD "ThelionkinG.1"
 
 //----Objects------
-
-//Define Firebase Data object
-FirebaseData fbdo;
-//Firbase authorization ibj
-FirebaseAuth auth;
-//Firebase configure object
-FirebaseConfig config;
-//string to store alarm
-/* The SMTP Session object used for Email sending */
-SMTPSession smtp;
+FirebaseData fbdo;//Define Firebase Data object
+FirebaseAuth auth;//Firbase authorization ibj
+FirebaseConfig config;//Firebase configure object
+SMTPSession smtp;/* The SMTP Session object used for Email sending */
 
 //--------variables-------
-
 //defining different varaibles to be used in the project
 String date_str;
 String time_str;
 int ir = 19;
-string alrm="";
+string alrm="";//global string variable to store alarm that is to store at firebase
+
+//--------array to store alarm--------
+String alarm1[3] = {"00:00:00","00:00:00","00:00:00"};
+String alarm2[3]  = {"00:00:00","00:00:00","00:00:00"};
+
+//---------RTC credentials==========
 const char *ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = 3600;
-const int daylightOffset_sec = 3600;
-
+const long  gmtOffset_sec = 18000;   // GMT offset (seconds)
+const int   daylightOffset_sec = 0; // daylight offset (seconds)
+//variable to make sure 1 day is decermented in 24 hours
+bool flag=false;
+AsyncWebServer server(80);//Defining port
+ 
 //----Functions-----
-
 /* Callback function to get the Email sending status */
 void smtpCallback(SMTP_Status status);
 //Function to send mail
+void sendMail();
+//functio nto conect ESp32 with wifi
+void connect_wifi();
+//function to return current local time
+String printLocalTime();
+//function to return current data day and year
+String printdate();
+//function to check if current local time mathes the alarm time
+String checktime();
+//function to send data to firebase
+void firebase_trigger(int a,bool status,String stats);
+//function for alarm
+void alarm(int a);
+//main setup function
+void setup()
+{
+  int remaining_days=30;
+  Serial.begin(115200);
+  pinMode(18, OUTPUT);  
+  pinMode(22, OUTPUT);
+  pinMode(ir, INPUT);
+  connect_wifi();
+  // init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+  //firebase
+  // Assign the api key 
+  config.api_key = API_KEY;
+  // Assign the RTDB URL 
+  config.database_url = DATABASE_URL;
+  auth.user.email=USER_EMAIL;
+  auth.user.password=USER_PASSWORD;
+  fbdo.setResponseSize(4096);
+  Firebase.reconnectWiFi(true);
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  Firebase.begin(&config, &auth);
+   
+   //routes for Web server
+  server.on("/alarm", HTTP_GET, [](AsyncWebServerRequest *request){
+ request->send(200, "text/html", "<h1>-------  Setting Alarm  ------</h1><form action='alarm' method='post'><label for='alarm1'>Select a time for Alarm1:</label>  <input type='time' id='alarm1' name='alarm1'><br><br><br>  <label for='alarm2'>Select a time for Alarm2:</label>  <input type='time' id='alarm2' name='alarm2'><br><br>  <input type='submit'></form>");
+  });
+   server.on("/alarm", HTTP_POST, [](AsyncWebServerRequest * request){
+    if(request->hasArg("alarm1" )&& request->hasArg("alarm2" )){
+        alarm1[0] = request->arg("alarm1")+":00" ;
+        alarm1[1] = request->arg("alarm1")+":01" ;
+        alarm1[2] = request->arg("alarm1")+":02" ;
+        alarm2[0] = request->arg("alarm2")+":00";
+        alarm2[1] = request->arg("alarm2")+":01";
+        alarm2[2] = request->arg("alarm2")+":02";
+        Serial.println("Alarm1 set at:"+ alarm1[0]);
+        Serial.println("Alarm2 set at:"+ alarm2[0]);
+    } 
+    request->send(200);
+ });
+
+  server.begin();
+}
+
+//main loop function
+void loop()
+{
+  delay(1000);
+  Serial.println(printLocalTime());
+  if (checktime()=="alarm1")
+  {
+    alarm(1);
+  }
+  else if(checktime()=="alarm2")
+  {
+    alarm(2);
+  }
+  else if ((digitalRead(ir)==HIGH))
+  {
+    digitalWrite(22,HIGH);
+    delay(500);
+    digitalWrite(22,LOW);
+    Serial.print("Not now");
+  }
+}
+
+String printLocalTime()
+{
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo))
+  {
+    Serial.println("Failed to obtain time");
+    return "Time Error";
+  }
+  char output[80];
+  strftime(output, 80, "%H:%M:%S", &timeinfo);
+  time_str = String(output);
+  return String(output);
+}
+
+String printdate()
+{
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo))
+  {
+    Serial.println("Failed to obtain date");
+    return "Date Error";
+  }
+  char output[80];
+  strftime(output, 80, "%B %d %Y", &timeinfo);
+  date_str = String(output);
+  return String(output);
+}
+
+String checktime()
+{
+  if (printLocalTime() == alarm1[0] || printLocalTime() == alarm1[1] || printLocalTime() == alarm1[2])
+    {
+    return "alarm1";
+    }
+  else if(printLocalTime() == alarm2[0] || printLocalTime() == alarm2[1] || printLocalTime() == alarm2[2])
+    {
+    return "alarm2";
+    }
+    else
+    return "";
+}
+
+void firebase_trigger(int a,bool status,String stats="")
+{
+ if (Firebase.ready()){
+    // Writing data on the database path 
+    if(a==1)//if alarm 1
+    {
+      Firebase.RTDB.setString(&fbdo, "ESP-data/"+printdate()+"/alarm1",alarm1[0] );
+      if(stats=="missed" && status==true)
+      {
+        Firebase.RTDB.setString(&fbdo, "ESP-data/"+printdate()+"/status1","missed" );
+        Firebase.RTDB.setString(&fbdo, "ESP-data/"+printdate()+"/time1"," ");
+      }
+      else 
+      {
+        Firebase.RTDB.setString(&fbdo, "ESP-data/"+printdate()+"/status1","taken" );
+        if(status==true && stats=="")
+          Firebase.RTDB.setString(&fbdo, "ESP-data/"+printdate()+"/time1","late");
+        else if(status==false && stats=="")
+          Firebase.RTDB.setString(&fbdo, "ESP-data/"+printdate()+"/time1","ontime");
+      }
+    }
+    
+    else if (a==2)//if alarm 2
+    {
+      Firebase.RTDB.setString(&fbdo, "ESP-data/"+printdate()+"/alarm2",alarm2[0] );
+      if(stats=="missed" && status==true)
+      {
+        Firebase.RTDB.setString(&fbdo, "ESP-data/"+printdate()+"/status2","missed" );
+        Firebase.RTDB.setString(&fbdo, "ESP-data/"+printdate()+"/time2"," ");
+      }
+      else 
+      {
+        Firebase.RTDB.setString(&fbdo, "ESP-data/"+printdate()+"/status2","taken" );
+        if(status==true && stats=="")
+          Firebase.RTDB.setString(&fbdo, "ESP-data/"+printdate()+"/time2","late");
+        else if(status==false && stats=="")
+          Firebase.RTDB.setString(&fbdo, "ESP-data/"+printdate()+"/time2","ontime");
+      }
+    
+    }
+     if(flag==false)
+     {
+      Firebase.RTDB.setInt(&fbdo, "ESP-data/"+printdate()+"/remainingdays",remaining_days);
+      Firebase.RTDB.getInt(&fbdo, "ESP-data/"+printdate()+"/remainingdays");
+      remaining_days = fbdo.intData();
+      remaining_days--;
+      flag=true;
+     }
+     else if(flag==true)
+      flag=false;
+ }
+}
+void alarm(int a)
+{
+  sendMail("Its time for you to take your Pills");
+  int time=millis();
+  while (digitalRead(ir) == LOW && millis()-time<= 20000)
+  {
+    digitalWrite(18, HIGH);
+    delay(1000);
+
+    digitalWrite(18, LOW);
+    delay(1000);
+    Serial.print(".");
+  }
+  if(digitalRead(ir)!=LOW)
+  {
+    digitalWrite(18, LOW);
+    if(millis()-time<10000)
+    {
+      firebase_trigger(a,false);
+      sendMail("Taken......Good....Pill taken on time :::::::: Remaining Days: "+remaining_days);
+      Serial.print("Taken early");
+    }
+    else 
+    {
+      firebase_trigger(a,true);
+      sendMail("Taken.....You were late::::::::Remaining Days: "+remaining_days);
+      Serial.print("Taken late");
+    }
+  }
+  else
+  {
+    
+    firebase_trigger(3,true,"missed");
+    sendMail("Missed.....now wait till the next alarm::::::::::Remaining Days: "+remaining_days);
+    Serial.print("You were very late");  
+  }
+}
+void connect_wifi()
+{
+  Serial.print("Connecting...");
+  WiFi.mode(WIFI_AP);
+  WiFi.begin(Wifi_net, Wifi_pass);
+  // timeout to stop checking for wifi connection after 20 seconds
+  unsigned long startattmpt = millis();
+  // to make sure were still within our timeout we will subtract startattempttime from current uptime and makesure its less then timeout
+  while (WiFi.status() != WL_CONNECTED && millis() - startattmpt < Timeout)
+  {
+    Serial.print(".");
+    delay(100);
+  }
+
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print("Failed");
+    // take any action
+  }
+  else
+  {
+    Serial.print("Connected");
+    Serial.println(WiFi.localIP());
+  }
+}
 void sendMail()
 {
 
@@ -98,125 +340,7 @@ void sendMail()
   if (!MailClient.sendMail(&smtp, &message))
     Serial.println("Error sending Email, " + smtp.errorReason());
 }
-//functio nto conect ESp32 with wifi
-void connect_wifi()
-{
-  Serial.print("Connecting...");
-  WiFi.mode(WIFI_AP);
-  WiFi.begin(Wifi_net, Wifi_pass);
-  // timeout to stop checking for wifi connection after 20 seconds
-  unsigned long startattmpt = millis();
-  // to make sure were still within our timeout we will subtract startattempttime from current uptime and makesure its less then timeout
-  while (WiFi.status() != WL_CONNECTED && millis() - startattmpt < Timeout)
-  {
-    Serial.print(".");
-    delay(100);
-  }
 
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print("Failed");
-    // take any action
-  }
-  else
-  {
-    Serial.print("Connected");
-    Serial.println(WiFi.localIP());
-  }
-}
-//function to return current local time
-String printLocalTime()
-{
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo))
-  {
-    Serial.println("Failed to obtain time");
-    return "Time Error";
-  }
-  char output[80];
-  strftime(output, 80, "%H:%M:%S", &timeinfo);
-  time_str = String(output);
-  return String(output);
-}
-
-//function to return current data day and year
-String printdate()
-{
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo))
-  {
-    Serial.println("Failed to obtain date");
-    return "Date Error";
-  }
-  char output[80];
-  strftime(output, 80, "%B %d %Y", &timeinfo);
-  date_str = String(output);
-  return String(output);
-}
-//main setup function
-void setup()
-{
-  Serial.begin(115200);
-  pinMode(18, OUTPUT);
-  pinMode(ir, INPUT);
-  connect_wifi();
-  // init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
-  //firebase
-  // Assign the api key 
-  config.api_key = API_KEY;
-  // Assign the RTDB URL 
-  config.database_url = DATABASE_URL;
-  auth.user.email=USER_EMAIL;
-  auth.user.password=USER_PASSWORD;
-  fbdo.setResponseSize(4096);
-  Firebase.reconnectWiFi(true);
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
-  Firebase.begin(&config, &auth);
-}
-
-//function to check if current local time mathes the alarm time
-bool checktime()
-{
-  if (printLocalTime() == "19:37:27" || printLocalTime() == "19:37:27" || printLocalTime() == "19:37:27")
-    alrm=printLocalTime();
-    return true;
-  else
-    return false;
-}
-void firebase_trigger(bool status)//sattus variable to tell whether pill was taken on time or not
-{
- if (Firebase.ready()){
-    // Writing data on the on real time database path
-    Firebase.RTDB.setInt(&fbdo, "ESP-data/"+printdate()+"/alarm1",alrm );
-    Firebase.RTDB.setInt(&fbdo, "ESP-data/"+printdate()+"/status","taken" );
-    if(status==true)
-    Firebase.RTDB.setInt(&fbdo, "ESP-data/"+printdate()+"/status1","late");
-    else
-    Firebase.RTDB.setInt(&fbdo, "ESP-data/"+printdate()+"/status1","ontime");
-
-}
-//function for alarm
-void alarm()
-{
-  int time=millis();//storing time to keep track of time
-  sendMail();//sending mail
-  while (digitalRead(ir) == LOW)
-  {
-    digitalWrite(18, HIGH);
-    delay(1000);
-
-    digitalWrite(18, LOW);
-    delay(1000);
-  }
-  digitalWrite(18, LOW);
-  if(millis()-time>10000)
-  firebase_trigger(false);//late
-  else 
-  firebase_trigger(true);//ontime
-}
 void smtpCallback(SMTP_Status status)
 {
   /* Print the current status */
@@ -237,13 +361,4 @@ void smtpCallback(SMTP_Status status)
     smtp.sendingResult.clear();
   }
 }
-//main loop function
-void loop()
-{
-  delay(1000);
-  Serial.println(printLocalTime());
-  if (checktime())
-  {
-    alarm();
-  }
-}
+
